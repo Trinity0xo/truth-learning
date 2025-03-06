64f08ddea1c4165d7b185c3b52c789bb29095165
= <=>
2025-03-06 09:31:27 +0700

    project initation

---
+++ src/main/java/com/truthlearning/truth/leaning/TruthLeaningApplication.java
@@ -0,0 +1,13 @@
+package com.truthlearning.truth.leaning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TruthLeaningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TruthLeaningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/truthlearning/truth/leaning/TruthLeaningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.truthlearning.truth.leaning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TruthLeaningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
